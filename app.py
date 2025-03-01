7b20edfc4f2a66a282128ade57a9ec26aa59b23c
omer bental <ombental@gmail.com>
2025-03-01 18:01:27 +0200

    learn about template rendering - basic

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, request, session, url_for
+from flask import Flask, redirect, render_template, request, session, url_for
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -9,20 +9,15 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 @app.route('/')
 def index():
     if 'username' in session:
-        return f'Logged in as {session["username"]}'
-    return 'You are not logged in'
+        return render_template("index_logged_in.html", username=session['username'])
+    return render_template("index_logged_out.html")
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         session['username'] = request.form['username']
         return redirect(url_for('index')) # TEACH ABOUT REDIRECT AND URL FOR!
-    return '''
-        <form method="post">
-            <p><input type=text name=username>
-            <p><input type=submit value=Login>
-        </form>
-    '''
+    return render_template("login.html")
 
 @app.route('/logout')
 def logout():
