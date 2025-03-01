1aba17e703a6ed9ca960acb726946cd288cba617
omer bental <ombental@gmail.com>
2025-03-01 17:54:39 +0200

    learn about sessions

--- app.py
+++ app.py
@@ -1,35 +1,31 @@
-from flask import Flask, request
+from flask import Flask, redirect, request, session, url_for
 from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/hello")
-def hello_world():
-    return "Hello, World!"
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "Index Page"
+    if 'username' in session:
+        return f'Logged in as {session["username"]}'
+    return 'You are not logged in'
 
-@app.route("/name/<username>")
-def name(username):
-    return f"Hi {escape(username)}"
-
-@app.route("/query")
-def query():
-    message = "You sent the following query params:"
-    for key, value in request.args.items():
-        message += f"\n{escape(key)}: {escape(value)}"
-    return message
-
-@app.route("/form", methods=["POST", "GET"])
-def form():
-    if request.method == "POST":
-        return f"You sent the username - {request.form['username']}"
-    elif request.method == "GET":
-        return '''
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index')) # TEACH ABOUT REDIRECT AND URL FOR!
+    return '''
         <form method="post">
             <p><input type=text name=username>
             <p><input type=submit value=Login>
         </form>
     '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index')) # TEACH ABOUT REDIRECT AND URL FOR!
