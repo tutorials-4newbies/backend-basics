80ee31e746fd00eb144251ff785831d91b7881e2
omer bental <ombental@gmail.com>
2025-03-01 17:41:16 +0200

    add route

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/hello")
 def hello_world():
     return "Hello, World!"
 
+@app.route("/")
+def index():
+    return "Index Page"
